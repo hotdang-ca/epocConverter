bccf6f50fa58f464b8a5d57dbcabe264650b4ea4
James Perih <james.perih@nerdsonsite.com>
2014-12-09 17:47:06 -0600

    Initial Commit

---
+++ EpochConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EpochConverter
+//
+//  Created by James Perih on 2014-12-09.
+//  Copyright (c) 2014 Hot Dang Interactive. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EpochConverter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EpochConverter
+//
+//  Created by James Perih on 2014-12-09.
+//  Copyright (c) 2014 Hot Dang Interactive. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
